a27b03ba8480e2d796bdfe6cae112c41eaeba229
L5vD5 <l5vd5@korea.ac.kr>
2019-09-25 20:35:12 +0900

    주문 수정 페이지 변경

--- src/views/Order/OrderModify.js
+++ src/views/Order/OrderModify.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardHeader, CardFooter, Col, Row, Label, Table } from 'reactstrap';
+import { Button, Input, Card, CardBody, CardHeader, CardFooter, Col, Row, Label, Table } from 'reactstrap';
+import ProductModal from './Modal';
+import Popup from "reactjs-popup";
+
+let d = {id: '', name: '', quantity: 0, price: 0, vos: 0, vat: 0, tax: false, sum: 0, tax: false};
 
 class OrderModify extends Component {
   constructor(props) {
@@ -8,7 +12,8 @@ class OrderModify extends Component {
     this.state = {
       data: {
         orderInfo: [{}],
-        productInfo: [{}]
+        productInfo: [{}],
+        name: '',
       }
     };
   }
@@ -24,10 +29,20 @@ class OrderModify extends Component {
       .then(data => {this.setState({data})});
   }
 
+  numberWithCommas(x) {
+    if(!x) {
+      console.log('Cannot convert');
+      return 0;
+    }
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   render() {
-    let {orderInfo} = this.state.data;
+    let {orderInfo, productInfo} = this.state.data;
     orderInfo = orderInfo[0];
-    let {productInfo} = this.state.data;
+    var d = new Date(orderInfo['date']);
+    var year = d.getFullYear(), month = d.getMonth()+1, date = d.getDate();
+
 
     return (
       <div className="animated fadeIn">
@@ -40,48 +55,39 @@ class OrderModify extends Component {
               <CardBody>
                 <Row>
                   <Col>
-                    <Label>거래처명 :&nbsp;</Label>
+                    <Label>고객명 :&nbsp;</Label><br />
                     <Label>{orderInfo['name']}</Label>
                   </Col>
                   <Col>
-                    <Label>일자 :&nbsp;</Label>
-                    <Label>{orderInfo['date']}</Label>
+                    <Label>일자 :&nbsp;</Label><br />
+                    <Label>{year}년 {month}월 {date}일</Label>
                   </Col>
                 </Row>
                 <Row>
-                  <Col>
-                    <Label>총액 :&nbsp;</Label>
-                    <Label>{orderInfo['price']}</Label>
-                  </Col>
-                  <Col>
-                    <Label>수금 :&nbsp;</Label>
-                    <Label>{orderInfo['received']}</Label>
-                  </Col>
+
                 </Row>
                 <Row>
                   <Col>
-                    <Label>상태 :&nbsp;</Label>
-                    <Label>{orderInfo['state']}</Label>
+                    <Label>전화번호 :&nbsp;</Label>
+                    <Input defaultValue={orderInfo['telephone']} onChange={(e) => {orderInfo['telephone'] = e.target.value}} />
                   </Col>
                   <Col>
-                    <Label>배송유형 :&nbsp;</Label>
-                    <Label>{}</Label>
+                    <Label>HP :&nbsp;</Label>
+                    <Input defaultValue={orderInfo['cellphone']} onChange={(e) => {orderInfo['cellphone'] = e.target.value}} />
                   </Col>
                 </Row>
                 <Row>
                   <Col>
                     <Label>배송지 :&nbsp;</Label>
-                    <Label>{}</Label>
+                    <Input defaultValue={orderInfo['address']} onChange={(e) => {orderInfo['address'] = e.target.value}} />
                   </Col>
                   <Col>
                     <Label>요청사항 :&nbsp;</Label>
-                    <Label>{}</Label>
+                    <Input defaultValue={orderInfo['comment']} onChange={(e) => {orderInfo['comment'] = e.target.value}} />
                   </Col>
                 </Row>
               </CardBody>
               <CardFooter>
-                {orderInfo['state'] === "order" ? <Button>출하 완료</Button> : null}
-                {orderInfo['state'] === "shipment" ? <Button>완료</Button> : null}
               </CardFooter>
             </Card>
           </Col>
@@ -90,25 +96,88 @@ class OrderModify extends Component {
           <Col>
           <Card>
             <CardHeader>
-              품목
+              <Row>
+                <Col md="10" xs="10" sm="10">품목을 입력하세요</Col>
+                <Col md="2" xs="2" sm="2">
+                  <Button block color="primary" 
+                    onClick={()=> {
+                      let sProduct = productInfo;
+                      sProduct.push(d);
+                      this.setState({
+                        productInfo: sProduct
+                      })}}>
+                    추가하기
+                  </Button>
+                </Col>  
+              </Row>
             </CardHeader>
             <CardBody>
             <Table>
                   <thead>
                     <tr>
-                      <th>제품명</th>
-                      <th>단가</th>
+                      <th>품목명</th>
                       <th>수량</th>
+                      <th>단가</th>
                       <th>공급가액</th>
+                      <th>부가세</th>
+                      <th>과세</th>
+                      <th>총액</th>
+                      <th>삭제</th>
                     </tr>
                   </thead>
                   <tbody>
                     {productInfo.map((e, i) => {
+                      console.log(e)
                       return ( <tr key={i}>
-                        <td>{e['name']}</td>
-                        <td>{e['price_shipping']}</td>
-                        <td>{e['quantity']}</td>
-                        <td>{e['price']}</td>
+                        <td>
+                          {<Popup
+                            trigger={<Input name='name' value={productInfo[i].name || ''} style={{cursor: 'pointer'}} onChange={() => console.log('S')}/>}
+                            modal>
+                            {close => <ProductModal index={i} close={close}
+                                        selectProduct={(data) => {
+                                          let sProduct = productInfo;
+
+                                          let val = Object.assign({}, sProduct[i]);
+                                      
+                                          /* set, for instance, comment[1] to "some text"*/
+                                          val['id'] = data['id'];
+                                          val['name'] = data['name'];
+                                          val['price_shipping'] = data['price_shipping'];
+                                      
+                                          sProduct[i] = val;
+                                          
+                                          /* set the state to the new variable */
+                                          console.log(productInfo, sProduct)
+                                          this.setState({productInfo: sProduct});
+                                        }}/>}
+                          </Popup>}
+                        </td>
+                        <td><Input name='quantity' value={productInfo[i].quantity || ''} onChange={(e)=> {
+                          let sProduct = productInfo;
+                          sProduct[i].quantity = e.target.value;
+                          this.setState({productInfo: sProduct})}}
+                        /></td>
+                        <td>{this.numberWithCommas(e['price_shipping'])}</td>
+                        <td>{this.numberWithCommas(Math.round(e['tax'] ? e['price_shipping'] * e['quantity'] * 10 / 11 : e['price_shipping'] * e['quantity']))}</td>
+                        <td>{this.numberWithCommas(Math.round(e['tax'] ? e['price_shipping'] * e['quantity'] * 1 / 11 : 0))}</td>
+                        <td><Input name='tax' type='checkbox' value={productInfo[i].tax} onClick={() => {
+                          let sProduct = productInfo;
+                          sProduct[i].tax = !sProduct[i].tax;
+                          this.setState({productInfo: sProduct})}} />
+                        </td>
+                        <td>{this.numberWithCommas(e['price_shipping'] * e['quantity'])}</td>
+                        <td>
+                          <Button block color="danger" 
+                            onClick={()=> {
+                              let sProduct = productInfo;
+                              sProduct.splice(i, 1);
+                              this.setState({
+                                productInfo: sProduct
+                              })
+                            }}>
+                            X
+                          </Button>
+                        </td>
                       </tr>
                       )
                     })}
@@ -119,6 +188,10 @@ class OrderModify extends Component {
           </Card>
           </Col>
         </Row>
+        <Button block color="primary" onClick={() => {
+          this.convertOrder(this.state);
+        }}>주문 수정하기</Button>
+
       </div>
     )
   }
--- src/views/Order/Transaction.js
+++ src/views/Order/Transaction.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import './Transaction.css'
 class Transaction extends Component {
   render() {
