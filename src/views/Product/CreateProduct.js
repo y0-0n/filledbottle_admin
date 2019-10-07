a50e0af07007a17734426641fb1eeaedf4ab422a
Jungsowon <soso2266@naver.com>
2019-10-07 17:42:03 +0900

    상품등록 이미지 보이기

--- src/views/Product/CreateProduct.js
+++ src/views/Product/CreateProduct.js
@@ -13,10 +13,10 @@ class CreateProduct extends Component {
       grade: '',
       weight: '',
       price: '',
-      selectedFile : null,
     };
 
     this.state = {
+      image : null,
       selectedFile : null,
     };
   }
@@ -24,7 +24,7 @@ class CreateProduct extends Component {
   componentWillMount() {
   }
 
-  addProduct(form) {
+  /*addProduct(form) {
     //const {name} = form;
     fetch(process.env.REACT_APP_HOST+"/product", {
       method: 'POST',
@@ -36,20 +36,40 @@ class CreateProduct extends Component {
     })
       .then(response => response.json())
       .then(data => {this.props.history.push('/main/product/list');});
-  }
+  }*/
 
   handleFileInput(e){
-    this.setState({
-      selectedFile : e.target.files[0],
-    })
+    var file = this.refs.file.files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function (e) {
+      this.setState({
+        image : [reader.result],
+      });
+    }.bind(this);
+
+    let img = e.target.files[0];
+
+    this.setState({img});
   }
 
   handlePost(){
-    const formData = new FormData();
+    let formData = new FormData();
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    };
+    formData.append('file', this.state.img);
+    for (let [key, value] of Object.entries(this.form)) {
+      //console.log(`${key}: ${value}`);
+      formData.append(key, value);
+    }
 
-    return axios.post("/api/upload", formData).then(res => {
-      alert('성공')
-    })(err=> {
+    return axios.post(process.env.REACT_APP_HOST+"/product", formData, config).then(res => {
+      alert('성공');
+      this.props.history.push('/main/product/list');
+    }).catch(err=> {
       alert('실패')
     })
   }
@@ -58,8 +78,8 @@ class CreateProduct extends Component {
     return (
       <div className="animated fadeIn">
         <Row className="mb-5">
-          <Col>
-            <Form onSubmit={(e) => {e.preventDefault(); this.addProduct(this.form)}}>
+          <Col md="12" xs="12" sm="12">
+            <form enctype="multipart/form-data" onSubmit={this.handlePost.bind(this)}>
               <FormGroup>
                 <Card>
                   <CardHeader>
@@ -91,7 +111,7 @@ class CreateProduct extends Component {
                       <tr className="TableBottom">
                         <th>사진</th>
                         <td colSpan="3" className="TableRight">
-                          <img style={{height: 500, width: 500}} src={this.state.selectedFile} alt="" /> <br></br>
+                        <img style={{height: 500, width: 500}} src={this.state.image} /> <br></br>
                         <input ref="file" type="file" name="file" onChange={e =>{this.handleFileInput(e);}}/> 
                         </td>
                       </tr>
@@ -103,7 +123,7 @@ class CreateProduct extends Component {
                   </CardFooter>
                 </Card>
               </FormGroup>
-            </Form>
+            </form>
           </Col>
         </Row>
       </div>
