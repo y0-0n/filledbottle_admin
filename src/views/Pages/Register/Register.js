dff55450d78b0dc32fb5b4b41beee7a3f371579a
L5vD5 <l5vd5@korea.ac.kr>
2019-10-08 17:13:49 +0900

    회원가입 추가

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.form = {
+      name: '',
+      id: '',
+      password: ''
+    }
+  }
+
+  componentWillMount() {
+  }
+
+  signup() {
+    fetch(process.env.REACT_APP_HOST+"/users/signup", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.form)
+    })
+      .then(response => response.json())
+      .then(data => {console.log(data);});
+  }
+  
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -11,21 +36,21 @@ class Register extends Component {
               <Card className="mx-4">
                 <CardBody className="p-4">
                   <Form>
-                    <h1>Register</h1>
-                    <p className="text-muted">Create your account</p>
+                    <h1>회원가입</h1>
+                    <p className="text-muted">정보를 입력하세요</p>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
                           <i className="icon-user"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Username" autoComplete="username" />
+                      <Input type="text" onChange={(e) => this.form.name=e.target.value} placeholder="이름" autoComplete="username" />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>@</InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Email" autoComplete="email" />
+                      <Input type="text" onChange={(e) => this.form.id=e.target.value} placeholder="아이디" autoComplete="email" />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
@@ -33,7 +58,7 @@ class Register extends Component {
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Password" autoComplete="new-password" />
+                      <Input type="password" onChange={(e) => this.form.password=e.target.value} placeholder="비밀번호" autoComplete="new-password" />
                     </InputGroup>
                     <InputGroup className="mb-4">
                       <InputGroupAddon addonType="prepend">
@@ -41,20 +66,20 @@ class Register extends Component {
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
+                      <Input type="password" placeholder="비밀번호 반복" autoComplete="new-password" />
                     </InputGroup>
-                    <Button color="success" block>Create Account</Button>
+                    <Button color="success" onClick={this.signup.bind(this)} block>가입하기</Button>
                   </Form>
                 </CardBody>
                 <CardFooter className="p-4">
-                  <Row>
+                  {/*<Row>
                     <Col xs="12" sm="6">
                       <Button className="btn-facebook mb-1" block><span>facebook</span></Button>
                     </Col>
                     <Col xs="12" sm="6">
                       <Button className="btn-twitter mb-1" block><span>twitter</span></Button>
                     </Col>
-                  </Row>
+                  </Row>*/}
                 </CardFooter>
               </Card>
             </Col>
