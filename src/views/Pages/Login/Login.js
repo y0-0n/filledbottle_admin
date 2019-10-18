9f529a035a90321af8facb2cfd55cfde443ef7fc
L5vD5 <l5vd5@korea.ac.kr>
2019-10-18 22:07:08 +0900

    로그인 구현

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -3,6 +3,38 @@ import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.form = {
+      email: '',
+      password: ''
+    }
+  }
+  login() {
+    
+    fetch(process.env.REACT_APP_HOST+"/api/auth/login", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(this.form)
+    })
+    .then(response => {
+      return Promise.all([response.status, response.json()]);
+    })
+    .then(data => {
+      let status = data[0], msg = data[1].message;
+      if(status == 200) {
+        console.log(data);
+        this.props.history.push('/');
+      } else {
+        alert('아이디 혹은 비밀번호가 잘못됐습니다.');
+      }
+    });
+  }
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -13,15 +45,15 @@ class Login extends Component {
                 <Card className="p-4">
                   <CardBody>
                     <Form>
-                      <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
+                      <h1>로그인</h1>
+                      <p className="text-muted">이메일 계정으로 로그인</p>
                       <InputGroup className="mb-3">
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
                             <i className="icon-user"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input type="text" placeholder="Username" autoComplete="username" />
+                        <Input type="text" name="email" onChange={(e) => this.form.email=e.target.value} placeholder="e-mail" autoComplete="username"/>
                       </InputGroup>
                       <InputGroup className="mb-4">
                         <InputGroupAddon addonType="prepend">
@@ -29,14 +61,14 @@ class Login extends Component {
                             <i className="icon-lock"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input type="password" placeholder="Password" autoComplete="current-password" />
+                        <Input type="password" name="password" onChange={(e) => this.form.password=e.target.value} placeholder="password" autoComplete="current-password" />
                       </InputGroup>
                       <Row>
                         <Col xs="6">
-                          <Button color="primary" className="px-4">Login</Button>
+                          <Button onClick={this.login.bind(this)} color="primary" className="px-4">로그인</Button>
                         </Col>
                         <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
+                          <Button color="link" className="px-0">비밀번호 분실</Button>
                         </Col>
                       </Row>
                     </Form>
@@ -45,11 +77,11 @@ class Login extends Component {
                 <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
                   <CardBody className="text-center">
                     <div>
-                      <h2>Sign up</h2>
+                      <h2>회원가입</h2>
                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua.</p>
                       <Link to="/register">
-                        <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
+                        <Button color="primary" className="mt-3" active tabIndex={-1}>지금 가입하기</Button>
                       </Link>
                     </div>
                   </CardBody>
