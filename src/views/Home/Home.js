dfa2ec2e8edc0e2a8977bdb21efb1a0b9c06581d
Jungsowon <soso2266@naver.com>
2019-11-08 20:07:46 +0900

    홈 화면 수정

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';                                                                                                                           
-import { Card, CardBody, CardHeader, Row, Col } from 'reactstrap';
+import { Card, CardBody, CardHeader, Row, Col, Table } from 'reactstrap';
 import moment from 'moment';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "moment/locale/ko";
@@ -40,6 +40,7 @@ class Home extends Component {
 
   componentWillMount() {
     this.getOrder();
+    this.getOnlyOrder();
     this.getIncome();
   }
 
@@ -93,6 +94,10 @@ class Home extends Component {
     })
   }
 
+  numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   getOrder() {
     fetch(process.env.REACT_APP_HOST+"/order/all/all/a", {
       method: 'GET',
@@ -135,7 +140,41 @@ class Home extends Component {
       });
   }
 
+  getOnlyOrder() {
+    fetch(process.env.REACT_APP_HOST+"/order/1/order/a", {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('token'),
+      }
+    })
+    .then(response => {
+      if(response.status === 401) {
+        return Promise.all([401])
+      } else {
+        return Promise.all([response.status, response.json()]);
+      }
+    })
+    .then(data => {
+      let status = data[0];
+      if(status === 200) {
+        let orderData = data[1];
+        this.setState({orderData})
+      } else {
+        alert('로그인 하고 접근해주세요')
+        this.props.history.push('/login')
+      }
+    });
+  }
+
+  getDate(dateInput) {
+    var d = new Date(dateInput);
+    var year = d.getFullYear(), month = d.getMonth()+1, date = d.getDate();
+
+    return year + "년 " + month + "월 " + date + "일";
+  }
+
   render() {
+    var data = this.state.search ? this.state.sdata : this.state.orderData;
     return (
       <div className="animated fadeIn">
         <Card>
@@ -165,6 +204,28 @@ class Home extends Component {
                 최근 주문
               </CardHeader>
               <CardBody>
+                <div>
+                  <Table hover>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>출하일</th>
+                        <th>고객</th>
+                        <th>총액</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {data.map((e, i) => {
+                        return (<tr style={{cursor: 'pointer'}} key={e.id} onClick={() => {this.props.history.push(`/main/sales/order/${e.id}`)}}>
+                        <td>{e.id}</td>
+                        <td>{this.getDate(e.date)}</td>
+                        <td>{e.name}</td>
+                        <td>{this.numberWithCommas(e.price)}</td>
+                        </tr>)
+                      })}
+                    </tbody>
+                  </Table>
+                </div>
               </CardBody>
             </Card>
           </Col>
