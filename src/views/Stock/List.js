548c1e66f6583e1063c9acd1a3df3db61040faa2
L5vD5 <l5vd5@korea.ac.kr>
2020-01-30 16:33:03 +0900

    재고 관리 모듈 pagination

--- src/views/Stock/List.js
+++ src/views/Stock/List.js
@@ -3,6 +3,8 @@ import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 import { Card, CardBody, CardHeader, Col, Row, Input, InputGroupAddon, Button, Nav, NavItem, NavLink, Table, CardFooter, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import DatePicker from "react-datepicker";
 
+const listCount = 15;
+
 class List extends Component {
   constructor(props) {
     super(props);
@@ -39,13 +41,47 @@ class List extends Component {
 			let status = data[0];
       if(status === 200) {
 				let stockData = data[1];
-        this.setState({stockData})
+				this.setState({stockData})
+				this.getTotal();
       } else {
         alert('로그인 하고 접근해주세요')
         this.props.history.push('/login')
       }
     });
 	}
+
+	getTotal() {
+    const {plant} = this.state;
+    fetch(process.env.REACT_APP_HOST + "/api/stock/total/", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token'),
+      },
+      body: JSON.stringify(
+        {
+          plant
+        }
+      )
+    })
+      .then(response => {
+        if (response.status === 401) {
+          return Promise.all([401])
+        } else {
+          return Promise.all([response.status, response.json()]);
+        }
+      })
+      .then(data => {
+        const status = data[0];
+        if (status === 200) {
+          this.setState({ total: Math.ceil(data[1][0].total / listCount) })
+        } else {
+          alert('로그인 하고 접근해주세요')
+          this.props.history.push('/login')
+        }
+      });
+  }
 	
 	getPlant(){
     fetch(process.env.REACT_APP_HOST+"/api/plant", {
@@ -94,8 +130,19 @@ class List extends Component {
 		})
 	}
 
+  countPageNumber(x) {
+    this.setState({
+      page: x,
+    }, () => {
+      this.getHistory();
+    });
+	}
+	
   render() {
 		const {plantData, stockData} = this.state;
+		const arr = [-2, -1, 0, 1, 2];
+    const arr1 = [];
+
     return (
       <div className="animated fadeIn">
 				<link rel="stylesheet" type="text/css" href="css/Table.css"></link>
@@ -150,6 +197,31 @@ class List extends Component {
 									</tbody>
 								</Table>
 							</CardBody>
+							<CardFooter>
+                <Pagination style={{justifyContent: 'center'}}>
+                  {this.state.page === 1 ? '' :
+                    <PaginationItem>
+                      <PaginationLink previous onClick={() => { this.countPageNumber(this.state.page - 1) }} />
+                    </PaginationItem>
+                  }
+                  {this.state.page === 1 ? arr.forEach(x => arr1.push(x + 2)) : null}
+                  {this.state.page === 2 ? arr.forEach(x => arr1.push(x + 1)) : null}
+                  {this.state.page !== 1 && this.state.page !== 2 ? arr.forEach(x => arr1.push(x)) : null}
+                  {arr1.map((e, i) => {
+                    if (this.state.total >= this.state.page + e)
+                      return (<PaginationItem key={i} active={this.state.page === this.state.page + e}>
+                        <PaginationLink onClick={() => { this.countPageNumber(this.state.page + e) }}>
+                          {this.state.page + e}
+                        </PaginationLink>
+                      </PaginationItem>)
+                    return null;
+                  })}
+                  {this.state.page === this.state.total ? '' :
+                    <PaginationItem>
+                      <PaginationLink next onClick={() => { this.countPageNumber(this.state.page + 1) }} />
+                    </PaginationItem>}
+                </Pagination>
+              </CardFooter>
             </Card>
           </Col>
         </Row>
