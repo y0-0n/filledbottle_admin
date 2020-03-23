dc907152ef069591f8b11c71663bd48c5cc0d610
L5vD5 <l5vd5@korea.ac.kr>
2020-03-23 16:05:17 +0900

    제조 날짜 설정할 수 있도록 변경

--- src/views/Manufacture/Create.js
+++ src/views/Manufacture/Create.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardBody, CardHeader, CardFooter, Col, Row, Table, Input } from 'reactstrap';
 import Popup from "reactjs-popup";
 import ProductModal from '../Modal/ProductModal';
+import DatePicker from "react-datepicker";
 import { registerLocale } from  "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import ko from 'date-fns/locale/ko';
@@ -22,7 +23,8 @@ class Create extends Component {
 			plantData: [[p]],
 			plantData2: [[p]],
       sProduct1: [d],//소모 상품
-      sProduct2: [d],//생산 상품
+			sProduct2: [d],//생산 상품
+			date: new Date(),
     };
   }
 
@@ -99,8 +101,8 @@ class Create extends Component {
   }
 
   produceProduct() {
-    const {sProduct1, sProduct2} = this.state;
-    console.warn(sProduct1, sProduct2)
+		const {sProduct1, sProduct2} = this.state;
+		const date = this.convertDateFormat(this.state.date)
     fetch(process.env.REACT_APP_HOST+"/api/manufacture", {
       method: 'POST',
       headers: {
@@ -108,7 +110,7 @@ class Create extends Component {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + localStorage.getItem('token'),
       },
-      body: JSON.stringify({sProduct1, sProduct2})
+      body: JSON.stringify({sProduct1, sProduct2, date})
     })
     .then(response => {
       if(response.status === 401) {
@@ -128,6 +130,10 @@ class Create extends Component {
         alert('에러로 인해 등록에 실패했습니다.')
       }
     });
+	}
+	
+  convertDateFormat(date) {
+    return date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
   }
 
   render() {
@@ -135,12 +141,107 @@ class Create extends Component {
       <div className="animated fadeIn">
       <link rel="stylesheet" type="text/css" href="css/Table.css"></link>
       <link rel="stylesheet" type="text/css" href="css/Manufacture.css"></link>
-        <Row>
+			<Row>
+				<Col md="12" xs="12" sm="12">
+            <Card>
+              <CardHeader>
+                <Row>
+								 <Col md="3" xs="2" sm="3">식품 가공</Col>
+								 <Col md="9" xs="10" sm="9">
+										<span className="date">
+											<DatePicker
+												className="datepicker"
+												dateFormat="yyyy년 MM월 dd일"
+												locale="ko"
+												selected={this.state.date}
+												onChange={(date) => { this.setState({ date }) }}
+											/>
+										</span>
+									</Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div style={{overflowX : "auto", whiteSpace: "nowrap"}}>
+                  <Table>
+                    <thead>
+                      <tr>
+                          <th>생산품목명<span style={{color : "#FA5858"}}> *</span></th>
+                          <th>창고<span style={{ color : "#FA5858"}}> *</span></th>
+                          <th>판매 단가</th>
+                          <th>수량<span style={{color : "#FA5858"}}> *</span></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        this.state.sProduct2.map(function (e, i) {
+                          return (
+                            <tr key={i}>
+                              <td>
+                                {<Popup
+                                  trigger={<Input name='name' value={this.state.sProduct2[i].name} style={{cursor: 'pointer', backgroundColor: '#ffffff'}} onChange={() => {console.log('S')}} readOnly/>}
+                                  modal>
+                                  {close => <ProductModal index={i} close={close} login={()=>{this.props.history.push('/login')}} createProduct={() => {this.props.history.push('/product/create')}}
+                                              selectProduct={(data) => {
+                                                let {sProduct2} = this.state;
+
+                                                let val = Object.assign({}, sProduct2[i]);
+
+                                                /* set, for instance, comment[1] to "some text"*/
+                                                val['id'] = data['id'];
+                                                val['name'] = data['name'];
+                                                val['price'] = data['price_shipping'];
+                                                val['grade'] = data['grade'];
+                                                val['weight'] = data['weight'];
+
+                                                sProduct2[i] = val;
+
+                                                /* set the state to the new variable */
+																								this.setState({sProduct2});
+																								this.getFamilyId(data['id'], i, 'manufacture');
+                                              }}
+                                            />}
+                                  </Popup>}
+                              </td>
+                              <td>
+                                <Input value={this.state.sProduct2[i].plant} onChange={(e) => {
+                                  let {sProduct2} = this.state;
+                                  sProduct2[i].plant = e.target.value;
+                                  this.setState({sProduct2})
+                                }} type='select' name="plant">
+                                  {
+                                    this.state.plantData2[i].map((e, i) => {
+                                      return <option key={i} value={e.id} >{e.name}</option>
+                                    })
+                                  }
+                                </Input>
+                              </td>
+                              <td><Input name='price' value={this.state.sProduct2[i].price} readOnly/></td>
+                              <td>
+                                <Input name='quantity' onChange={(e) => {
+                                  let {sProduct2} = this.state;
+                                  sProduct2[i] = Object.assign({}, sProduct2[i]);
+                                  sProduct2[i].quantity = e.target.value;
+                                  this.setState({sProduct2})
+                                  }
+                                } />
+                              </td>
+                            </tr>
+                          )
+                        }, this)
+                      }
+                    </tbody>
+                  </Table>
+                </div>
+              </CardBody>
+              <CardFooter>
+              </CardFooter>
+            </Card>
+          </Col>
           <Col md="12" xs="12" sm="12">
           <Card>
               <CardHeader>
                 <Row>
-                  <Col>소모 상품</Col>
+                  <Col>소모 품목</Col>
                   <Col>
                     <div style={{float:"right"}}>
                       <Button block color="primary"
@@ -162,10 +263,10 @@ class Create extends Component {
                   <Table>
                     <thead>
                       <tr>
-                          <th>상품명<span style={{color : "#FA5858"}}> *</span></th>
+                          <th>품목명<span style={{color : "#FA5858"}}> *</span></th>
                           <th>창고<span style={{color : "#FA5858"}}> *</span></th>
                           <th>단가</th>
-                          <th>소모재고<span style={{color : "#FA5858"}}> *</span></th>
+                          <th>수량<span style={{color : "#FA5858"}}> *</span></th>
                           <th>삭제</th>
                       </tr>
                     </thead>
@@ -236,90 +337,6 @@ class Create extends Component {
                                   }}>
                                   X
                                 </Button>
-                              </td>
-                            </tr>
-                          )
-                        }, this)
-                      }
-                    </tbody>
-                  </Table>
-                </div>
-              </CardBody>
-              <CardFooter>
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col md="12" xs="12" sm="12">
-            <Card>
-              <CardHeader>
-                <Row>
-                  <Col>생산 상품</Col>
-                </Row>
-              </CardHeader>
-              <CardBody>
-                <div style={{overflowX : "auto", whiteSpace: "nowrap"}}>
-                  <Table>
-                    <thead>
-                      <tr>
-                          <th>상품명<span style={{color : "#FA5858"}}> *</span></th>
-                          <th>창고<span style={{ color : "#FA5858"}}> *</span></th>
-                          <th>단가</th>
-                          <th>제조량<span style={{color : "#FA5858"}}> *</span></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        this.state.sProduct2.map(function (e, i) {
-                          return (
-                            <tr key={i}>
-                              <td>
-                                {<Popup
-                                  trigger={<Input name='name' value={this.state.sProduct2[i].name} style={{cursor: 'pointer', backgroundColor: '#ffffff'}} onChange={() => {console.log('S')}} readOnly/>}
-                                  modal>
-                                  {close => <ProductModal index={i} close={close} login={()=>{this.props.history.push('/login')}} createProduct={() => {this.props.history.push('/product/create')}}
-                                              selectProduct={(data) => {
-                                                let {sProduct2} = this.state;
-
-                                                let val = Object.assign({}, sProduct2[i]);
-
-                                                /* set, for instance, comment[1] to "some text"*/
-                                                val['id'] = data['id'];
-                                                val['name'] = data['name'];
-                                                val['price'] = data['price_shipping'];
-                                                val['grade'] = data['grade'];
-                                                val['weight'] = data['weight'];
-
-                                                sProduct2[i] = val;
-
-                                                /* set the state to the new variable */
-																								this.setState({sProduct2});
-																								this.getFamilyId(data['id'], i, 'manufacture');
-                                              }}
-                                            />}
-                                  </Popup>}
-                              </td>
-                              <td>
-                                <Input value={this.state.sProduct2[i].plant} onChange={(e) => {
-                                  let {sProduct2} = this.state;
-                                  sProduct2[i].plant = e.target.value;
-                                  this.setState({sProduct2})
-                                }} type='select' name="plant">
-                                  {
-                                    this.state.plantData2[i].map((e, i) => {
-                                      return <option key={i} value={e.id} >{e.name}</option>
-                                    })
-                                  }
-                                </Input>
-                              </td>
-                              <td><Input name='price' value={this.state.sProduct2[i].price} readOnly/></td>
-                              <td>
-                                <Input name='quantity' onChange={(e) => {
-                                  let {sProduct2} = this.state;
-                                  sProduct2[i] = Object.assign({}, sProduct2[i]);
-                                  sProduct2[i].quantity = e.target.value;
-                                  this.setState({sProduct2})
-                                  }
-                                } />
                               </td>
                             </tr>
                           )
