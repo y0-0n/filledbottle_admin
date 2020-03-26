f20e92d9f87d1126ed1479b038d5eb9d0304c1ea
sowish23 <soso2266@naver.com>
2020-03-26 16:50:31 +0900

    admin user detail 충돌 수정

--- src/views/Admin/Users/Detail.js
+++ src/views/Admin/Users/Detail.js
@@ -7,29 +7,15 @@ class UserListDetail extends Component {
     super(props);
     this.state = {
       data: [[]],
-      plantData: [],
-      familyInPlantData: [],
-      allFamilyData: [],
       familyData: [],
-      categoryData: [],
-      category: 1,
-      addFamilyList: [],
-      deleteFamilyList: [],
-      family: 1,
-      productData: [],
-      prohibitDelete: false,
-      plant: 1,
-      deletePlantList: [],
     }
   }
+  
   initMap() {
     let map = new naver.maps.Map('map', {
         center: new naver.maps.LatLng(this.state.y, this.state.x),
-        zoom: 15
+        zoom: 18
     });
-  }
-  componentDidMount() {
-    console.log(this.state.data)
   }
 
   getCoord() {
@@ -48,7 +34,7 @@ class UserListDetail extends Component {
   }
 
   getDetail() {
-    fetch(process.env.REACT_APP_HOST + "/api/auth/info", {
+    fetch(process.env.REACT_APP_HOST +"/api/admin/users/detail/"+this.props.match.params.id, {
       method: 'GET',
       headers: {
         'Authorization': 'Bearer ' + localStorage.getItem('token'),
@@ -74,92 +60,8 @@ class UserListDetail extends Component {
       })
   }
 
-  getPlantList() {
-    fetch(process.env.REACT_APP_HOST + "/api/plant", {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-      },
-    })
-      .then(response => {
-        if (response.status === 401) {
-          return Promise.all([401])
-        } else {
-          return Promise.all([response.status, response.json()]);
-        }
-      })
-      .then(data => {
-        let status = data[0];
-        if (status === 200) {
-          this.setState({ plantData: data[1] });
-          data[1].map((e, i) => {
-            this.getFamilyInPlant(e.id, i);
-          })
-        }
-        else {
-          alert('로그인 하고 접근해주세요');
-          this.props.history.push('/login');
-        }
-      });
-  }
-
-  getFamilyInPlant(plantId, i) {
-    fetch(process.env.REACT_APP_HOST + "/api/product/familyInPlant/" + plantId, {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-      },
-    })
-      .then(response => {
-        if (response.status === 401) {
-          return Promise.all([401])
-        } else {
-          return Promise.all([response.status, response.json()]);
-        }
-      })
-      .then(data => {
-        let status = data[0];
-        if (status === 200) {
-          let { familyInPlantData } = this.state;
-          familyInPlantData[i] = data[1];
-          this.setState({ familyInPlantData });
-        }
-        else {
-          alert('로그인 하고 접근해주세요');
-          this.props.history.push('/login');
-        }
-      });
-  }
-
-  getAllFamily() {
-    fetch(process.env.REACT_APP_HOST + "/api/product/allFamily/" + this.state.category, {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-      },
-    })
-      .then(response => {
-        if (response.status === 401) {
-          return Promise.all([401])
-        } else {
-          return Promise.all([response.status, response.json()]);
-        }
-      })
-      .then(data => {
-        let status = data[0];
-        if (status === 200) {
-          this.setState({ allFamilyData: data[1] });
-          this.getProductFamily();
-        }
-        else {
-          alert('로그인 하고 접근해주세요');
-          this.props.history.push('/login');
-        }
-      });
-  }
-
   getProductFamily() {
-    fetch(process.env.REACT_APP_HOST + "/api/product/familyList/" + this.state.category, {
+    fetch(process.env.REACT_APP_HOST + "/api/admin/users/productFamily/" + this.props.match.params.id, {
       method: 'GET',
       headers: {
         'Authorization': 'Bearer ' + localStorage.getItem('token'),
@@ -175,45 +77,13 @@ class UserListDetail extends Component {
       .then(data => {
         let status = data[0];
         if (status === 200) {
-          this.setState({ familyData: data[1] });
-          //이 부분 나중에 콜백으로 바꾸기
-          this.setState({
-            addFamilyList: [],
-            deleteFamilyList: [],
-          }, () => {
-          });
+          this.setState({familyData: data[1]})
         }
         else {
           alert('로그인 하고 접근해주세요');
           this.props.history.push('/login');
         }
-      })
-  }
-
-  getFamilyCategory() {
-    fetch(process.env.REACT_APP_HOST + "/api/product/familyCategory", {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-      },
-    })
-      .then(response => {
-        if (response.status === 401) {
-          return Promise.all([401])
-        } else {
-          return Promise.all([response.status, response.json()]);
-        }
-      })
-      .then(data => {
-        let status = data[0];
-        if (status === 200) {
-          this.setState({ categoryData: data[1] });
-        }
-        else {
-          alert('로그인 하고 접근해주세요');
-          this.props.history.push('/login');
-        }
-      })
+      });
   }
 
   tabClick(category) {
@@ -226,59 +96,11 @@ class UserListDetail extends Component {
 
   componentWillMount() {
     this.getDetail();
-    this.getPlantList();
-    this.getAllFamily();
-    this.getFamilyCategory();
-  }
-
-
-
-  async getProduct() {
-    const { family, category } = this.state;
-    let result = false;
-    await fetch(process.env.REACT_APP_HOST + "/product/list", {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-      },
-      body: JSON.stringify(
-        {
-          name: '', page: 'all', family, category
-        }
-      )
-    })
-      .then(response => {
-        if (response.status === 401) {
-          return Promise.all([401])
-        } else {
-          return Promise.all([response.status, response.json()]);
-        }
-      })
-      .then(data => {
-        let status = data[0];
-        if (status === 200)
-          this.setState({ productData: data[1] }, () => {
-            console.warn(this.state.productData.length)
-            if (this.state.productData.length) {
-              alert("품목이 존재함으로 삭제할 수 없습니다.");
-              result = false;
-            }
-            else {
-              result = true;
-            }
-          });
-        else {
-          alert('로그인 하고 접근해주세요');
-          this.props.history.push('/login');
-        }
-      })
-    return result;
+    this.getProductFamily();
   }
 
   render() {
-    const { data, plantData, allFamilyData, familyData, categoryData, productData } = this.state;
+    const { data, familyData, } = this.state;
     return (
       <div className="animated fadeIn">
         <link rel="stylesheet" type="text/css" href="css/Table.css"></link>
@@ -341,64 +163,17 @@ class UserListDetail extends Component {
             <Card>
               <CardHeader>
                 <Row>
-                  <Col>창고</Col>
-                </Row>
-              </CardHeader>
-              <CardBody>
-                <Table className="ShowTable">
-                  <thead>
-                    <tr>
-                      <th>창고명</th>
-                      <th>저장 품목군</th>
-                      {/*<th>저장량</th>*/}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {plantData.map((e, i) => {
-                      return (<tr key={i}>
-                        <td>{e.name}</td>
-                        <td>
-                          {
-                            this.state.familyInPlantData[i] !== undefined ?
-                              this.state.familyInPlantData[i].map((e, i) => {
-                                return e.name
-                              }) : null}
-                        </td>
-                      </tr>)
-                    }
-                    )}
-                    <tr>
-                    </tr>
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-          </Col>
-
-          <Col md="12" xs="12" sm="12">
-            <Card>
-              <CardHeader>
-                <Row>
                   <Col>취급 품목</Col>
                 </Row>
               </CardHeader>
               <CardBody>
-                <Nav tabs>
-                  {categoryData.map((e, i) => {
-                    return <NavItem key={i}>
-                      <NavLink active={this.state.category === e.id} onClick={() => this.tabClick(e.id)} href="#">{e.name}</NavLink>
-                    </NavItem>
-                  })}
-                </Nav>
                 <div>
                   <ul className="ul-productFamily" style={{ listStyleType: "none", }}>
-                    {allFamilyData.map((e, i) => {
-                      const f = (element) => element.id === e.id
+                    {familyData.map((e, i) => {
                       return (
                         <li key={i} className="list-productFamily" style={{
-                          color: familyData.findIndex(f) === -1 ? this.state.addFamilyList.findIndex(f) === -1 ? 'black' : '#fff' : this.state.deleteFamilyList.findIndex(f) !== -1 ? 'black' : '#fff',
-                          backgroundColor: familyData.findIndex(f) === -1 ? this.state.addFamilyList.findIndex(f) === -1 ? '#fff' : '#20A8D8' : this.state.deleteFamilyList.findIndex(f) !== -1 ? '#fff' : '#20A8D8',
-                          borderColor: familyData.findIndex(f) === -1 ? this.state.addFamilyList.findIndex(f) === -1 ? 'lightgray' : '' : this.state.deleteFamilyList.findIndex(f) !== -1 ? 'lightgray' : ''
+                          color: '#fff',
+                          backgroundColor: '#20A8D8',
                         }}>{e.name}</li>
                       )
                     }
