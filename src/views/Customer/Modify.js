192b8bac3fd2a67b18fbc0489dc65a2678f7ee5f
parkyolo <20181621@kookmin.ac.kr>
2020-04-01 15:00:54 +0900

    고객관리 주소, 재고실사 수정

--- src/views/Customer/Modify.js
+++ src/views/Customer/Modify.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardHeader, CardFooter, Col, Row, FormGroup, Input, Table, Badge} from 'reactstrap';
+import { Button, Card, CardBody, CardHeader, CardFooter, Col, Row, FormGroup, Input, Table, InputGroupAddon, InputGroup, Badge} from 'reactstrap';
 
 class Modify extends Component {
   constructor(props) {
@@ -35,11 +35,45 @@ class Modify extends Component {
       });
   }
 
+  sample6_execDaumPostcode() {
+    new window.daum.Postcode({
+        oncomplete: function(data) {
+            var addr = ''; // 주소 변수
+            var extraAddr = ''; // 참고항목 변수
+            if (data.userSelectedType === 'R') {
+                addr = data.roadAddress;
+            } else {
+                addr = data.jibunAddress;
+            }
+            if(data.userSelectedType === 'R'){
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraAddr += data.bname;
+                }
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                if(extraAddr !== ''){
+                    extraAddr = ' (' + extraAddr + ')';
+                }
+                //document.getElementById("sample6_extraAddress").value = extraAddr;
+								addr += extraAddr;
+            } else {
+                //document.getElementById("sample6_extraAddress").value = '';
+            }
+            document.getElementById('sample6_postcode').value = data.zonecode;
+            document.getElementById("sample6_address").value = addr;
+            document.getElementById("sample6_detailAddress").focus();
+        }
+    }).open();
+  }
+
   handlePost(e) {
     e.preventDefault();
     let c = window.confirm('이 상품을 수정하시겠습니까?')
     if(c) {
       let formData = new FormData();
+      this.form.address = document.getElementById("sample6_address").value+" "+document.getElementById("sample6_detailAddress").value;
+		  this.form.postcode = document.getElementById("sample6_postcode").value;
       for (let [key, value] of Object.entries(this.form)) {
         formData.append(key, value);
       }
@@ -102,13 +136,13 @@ class Modify extends Component {
                         <tr>
                           <th>연락처 1</th>
                           <td>
-                            <Input defaultValue={data.telephone} onChange={(e) => this.form.cellphone=e.target.value}/>
+                            <Input defaultValue={data.cellphone} onChange={(e) => this.form.cellphone=e.target.value}/>
                           </td>
                         </tr>
                         <tr>
                           <th>연락처 2</th>
                           <td>
-                            <Input defaultValue={data.cellphone} onChange={(e) => this.form.telephone=e.target.value}/>
+                            <Input defaultValue={data.telephone} onChange={(e) => this.form.telephone=e.target.value}/>
                           </td>
                         </tr>
                         <tr>
@@ -119,8 +153,29 @@ class Modify extends Component {
                         </tr>
                         <tr>
                           <th>주소</th>
-                          <td>
-                            <Input defaultValue={data.address} onChange={(e) => this.form.address=e.target.value}/>
+                          <td colSpan="3">
+                            <Row style={{marginBottom: '10px'}}>
+                              <Col lg="6" md="6" sm="6">
+                                <div style={{marginBottom: '10px', marginLeft: '5px'}}>{data.address}</div>
+                                <InputGroup required>
+                                  <Input type="text" id="sample6_postcode" placeholder="우편번호" readOnly/>                            
+                                  <InputGroupAddon addonType="append">
+                                    <Button block color="primary" onClick={() => {this.sample6_execDaumPostcode()}}>우편번호찾기</Button>
+                                  </InputGroupAddon>
+                                </InputGroup>
+                              </Col>
+                            </Row>
+                            <Row style={{marginBottom: '10px'}}>
+                              <Col>
+                                <Input type="text" id="sample6_address" placeholder="주소" readOnly/>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col lg="6" md="6" sm="6" style={{paddingRight: '0px'}}>
+                                <Input type="text" id="sample6_detailAddress" placeholder="상세주소"/>
+                              </Col>
+                            </Row>
+                            {/*<Input onChange={(e) => this.form.address=e.target.value}/>*/}
                           </td>
                         </tr>
                       </tbody>
