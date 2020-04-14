66315e7d77ea93554fbefad16aebebad8e5c822a
L5vD5 <l5vd5@korea.ac.kr>
2020-04-14 17:09:21 +0900

    설문조사 DB에서 질문 가져오기

--- src/views/Survey/Survey.js
+++ src/views/Survey/Survey.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import { Button, Card, CardBody, CardHeader, CardFooter, Col, Row, Input, Table, } from 'reactstrap';
 
-const questionCount = 3
-
 class Create extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      question_no : 1,
+			question_no : 1,
+			data: []
     };
   }
 
   componentWillMount() {
+		this.getQuestionList();
+	}
+	
+	getQuestionList() {
+    fetch(process.env.REACT_APP_HOST + "/api/survey/", {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('token'),
+      },
+    })
+      .then(response => {
+        if (response.status === 401) {
+          return Promise.all([401])
+        } else {
+          return Promise.all([response.status, response.json()]);
+        }
+      })
+      .then(data => {
+        let status = data[0];
+        if (status === 200)
+          this.setState({ data: data[1] });
+        else {
+          alert('로그인 하고 접근해주세요');
+          this.props.history.push('/login');
+        }
+      })
   }
+
 
   prevQuestion() {
     console.log(this.state.question_no)
@@ -26,17 +52,17 @@ class Create extends Component {
   }
 
   nextQuestion() {
-    console.log(this.state.question_no)
-    if (this.state.question_no >= questionCount) {
+    if (this.state.question_no >= this.state.data.length) {
         alert(this.state.question_no)
     } else {
-        this.setState({
-          question_no : this.state.question_no + 1,
-        })
+			this.setState({
+				question_no : this.state.question_no + 1,
+			})
     }
   }
 
   render() {
+		const data = this.state.data;
     return (
       <div className="animated fadeIn">
         <link rel="stylesheet" type="text/css" href="css/Survey.css"></link>
@@ -47,112 +73,48 @@ class Create extends Component {
                 설문 조사
                   </CardHeader>
               <CardBody>
+								<div className="survey_explain">
+									다음은 귀하의 기분을 묻는 질문입니다. <br></br>
+									지난 2주간 각 문항에 해당하는 증상들을어느 정도로경험하였는지 해당하는 번호에 표시하여 주세요.
+								</div>
                 <div className="survey_card">
-                  <div className="survey_explain">
-                    다음은 귀하의 기분을 묻는 질문입니다. <br></br>
-                    지난 2주간 각 문항에 해당하는 증상들을어느 정도로경험하였는지 해당하는 번호에 표시하여 주세요.
-                  </div>
-                  {this.state.question_no === 1 ? 
-                  <div className="survey_content">
-                    <div className="content_question">
-                      <span>질문 : </span>
-                      <span>(지난 2주간) 하루 중 대부분의 시간동안 울적했다.</span>
-                    </div>
-                    <div className="content_answers">
-                      <label for="q1-1">
-                        <input id="q1-1" type="radio" name="q1" />
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 1</span>
-                        </div>
-                      </label>
-                      <label for="q1-2">
-                        <input id="q1-2" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 2</span>
-                        </div>
-                      </label>
-                      <label for="q1-3">
-                        <input id="q1-3" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 3</span>
-                        </div>
-                      </label>
-                      <label for="q1-4">
-                        <input id="q1-4" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 4</span>
-                        </div>
-                      </label>
-                    </div>
-                  </div> : ""}
-
-                  {this.state.question_no === 2 ? 
-                  <div className="survey_content">
-                    <div className="content_question">
-                      <span>질문 : </span>
-                      <span>두번째 질무ㄴ이다 하루 중 대부분의 시간동안 울적했다.</span>
-                    </div>
-                    <div className="content_answers">
-                      <label for="q1-1">
-                        <input id="q1-1" type="radio" name="q1" />
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 1</span>
-                        </div>
-                      </label>
-                      <label for="q1-2">
-                        <input id="q1-2" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 2</span>
-                        </div>
-                      </label>
-                      <label for="q1-3">
-                        <input id="q1-3" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 3</span>
-                        </div>
-                      </label>
-                      <label for="q1-4">
-                        <input id="q1-4" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 4</span>
-                        </div>
-                      </label>
-                    </div>
-                  </div> : ""}
-
-                  {this.state.question_no === 3 ? 
-                  <div className="survey_content">
-                    <div className="content_question">
-                      <span>질문 : </span>
-                      <span>3번째 질문 하루 중 대부분의 시간동안 울적했다.</span>
-                    </div>
-                    <div className="content_answers">
-                      <label for="q1-1">
-                        <input id="q1-1" type="radio" name="q1" />
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 1</span>
-                        </div>
-                      </label>
-                      <label for="q1-2">
-                        <input id="q1-2" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 2</span>
-                        </div>
-                      </label>
-                      <label for="q1-3">
-                        <input id="q1-3" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 3</span>
-                        </div>
-                      </label>
-                      <label for="q1-4">
-                        <input id="q1-4" type="radio" name="q1"/>
-                        <div className="answer_box">
-                          <span>질문에 대한 대답 4</span>
-                        </div>
-                      </label>
-                    </div>
-                  </div> : ""}
+									{
+										data.map((e, i) => {
+											return (
+												<div style={{display:(this.state.question_no-1 === i ? "block" : "none")}} className="survey_content">
+													<div className="content_question">
+														<span>질문 : </span>
+														<span>{e.question}</span>
+													</div>
+													<div className="content_answers">
+														<label for={"q"+i+"-1"}>
+															<input id={"q"+i+"-1"} type="radio" name="q1" />
+															<div className="answer_box">
+																<span>{e.answer1}</span>
+															</div>
+														</label>
+														<label for={"q"+i+"-2"}>
+															<input id={"q"+i+"-2"} type="radio" name="q1"/>
+															<div className="answer_box">
+																<span>{e.answer2}</span>
+															</div>
+														</label>
+														<label for={"q"+i+"-3"}>
+															<input id={"q"+i+"-3"} type="radio" name="q1"/>
+															<div className="answer_box">
+																<span>{e.answer3}</span>
+															</div>
+														</label>
+														<label for={"q"+i+"-4"}>
+															<input id={"q"+i+"-4"} type="radio" name="q1"/>
+															<div className="answer_box">
+																<span>{e.answer4}</span>
+															</div>
+														</label>
+													</div>
+												</div>)
+										})
+									}
                   <div className="survey_button">
                     <Button color="ghost-primary" className="prev_button" onClick={() => this.prevQuestion()}>이전</Button>
                     <Button color="primary" className="next_button" onClick={() => this.nextQuestion()}>다음</Button>
