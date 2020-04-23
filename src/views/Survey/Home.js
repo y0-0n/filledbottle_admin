b5821715e333c44aeb7c838014f7f55a8a19efbd
sowish23 <soso2266@naver.com>
2020-04-23 17:24:02 +0900

    survey/home react hooks

--- src/views/Survey/Home.js
+++ src/views/Survey/Home.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button, Row, Col, Card, CardFooter, CardHeader, CardBody } from 'reactstrap';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-			surveyStart: true,
-    };
-  }
+function Home(props) {
+  const [survey, setSurvey] = useState(true)
 
-  render() {
+  if (survey === true) {
     return (
       <div className="animated fadeIn">
         <link rel="stylesheet" type="text/css" href="css/Survey.css"></link>
@@ -20,27 +15,41 @@ class Home extends Component {
                 <div>설문조사</div>
               </CardHeader>
               <CardBody>
-                {this.state.surveyStart ? 
-                  <div style={{textAlign : 'center'}}>
-                    <div style={{marginBottom : 20}}>
-                      설문조사 시작하기
-                    </div>
-                    <div>
-                      <Button color="primary" onClick={() => this.props.history.push('/main/survey')}>시작하기</Button>
-                    </div>
-                  </div> 
-                  :
-                  <div style={{textAlign: 'center'}}>
-                    <div>설문조사 결과 페이지</div>
+                <div style={{ textAlign: 'center' }}>
+                  <div style={{ marginBottom: 20 }}>
+                    설문조사 시작하기
                   </div>
-                }
+                  <div>
+                    <Button color="primary" onClick={() => props.history.push('/main/survey')}>시작하기</Button>
+                  </div>
+                </div>
               </CardBody>
-
             </Card>
           </Col>
         </Row>
       </div>
-    );
+    )
+  }
+  else {
+    return (
+      <div className="animated fadeIn">
+        <link rel="stylesheet" type="text/css" href="css/Survey.css"></link>
+        <Row className="mb-5">
+          <Col md="12" xs="12" sm="12">
+            <Card>
+              <CardHeader>
+                <div>설문조사</div>
+              </CardHeader>
+              <CardBody>
+                <div style={{ textAlign: 'center' }}>
+                  <div>설문조사 결과 페이지</div>
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    )
   }
 }
 
