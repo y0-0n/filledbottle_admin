ad7a0148983b5a544da86caf66a58122ece7bab0
sowish23 <soso2266@naver.com>
2020-06-16 18:02:20 +0900

    재고 등록 페이지 만듦

--- src/views/Stock/Create.js
+++ src/views/Stock/Create.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardHeader, CardFooter, Col, Row, FormGroup, Input, Table } from 'reactstrap';
+import { Button, Card, CardBody, CardHeader, CardFooter, Col, Row, FormGroup, Input, Table, InputGroup, InputGroupAddon } from 'reactstrap';
 import ProductFamilyModal from '../Modal/ProductFamilyModal';
 import ProductModal from '../Modal/ProductModal';
 import Popup from "reactjs-popup";
+import DatePicker from "react-datepicker";
+
 
 class Create extends Component {
   constructor(props) {
@@ -119,7 +121,84 @@ class Create extends Component {
 		console.log(this.state.current)
     return (
       <div className="animated fadeIn align-items-center">
-      <link rel="stylesheet" type="text/css" href="css/Table.css"></link>
+        <link rel="stylesheet" type="text/css" href="css/CreateCopy.css"></link>
+        <div className="form-card">
+          <div className="form-title">재고 등록</div>
+          <div className="form-innercontent">
+            <div className="sell-list">
+              <div className="sell-content">
+                <label className="sell-label">품목명</label>
+                <div className="sell-input">
+                  <Input style={{width: "30%"}}/>
+                </div>
+              </div>
+              <div className="sell-discount">
+                <div style={{display: "inline-block"}}>
+                  <img src="/assets/img/noimage.jpg"/>
+                </div>
+                <div style={{display: "inline-block"}}>
+                  <div className="product-info">
+                    <label>카테고리</label>
+                    <input></input>
+                  </div>
+                  <div className="product-info">
+                    <label>품목군</label>
+                    <input></input>
+                  </div>
+                  <div className="product-info">
+                    <label>판매단가</label>
+                    <input></input>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="sell-list">
+              <label className="sell-label">구분</label>
+              <div className="sell-input">
+                <Input style={{width: "30%"}} readOnly/>
+              </div>
+            </div>
+            <div className="sell-list">
+              <div className="sell-content">
+                <label className="sell-label">재고수</label>
+                <div className="sell-input">
+                  <InputGroup>
+                    <Input type="number" placeholder="숫자만 입력" required onChange={(e) => this.form.price = e.target.value/*this.changePrice.bind(this)*/} />
+                    <InputGroupAddon addonType="append">
+                      개
+                    </InputGroupAddon>
+                  </InputGroup>
+                </div>
+              </div>
+            </div>
+            <div className="sell-list">
+              <label className="sell-label">유통기한</label>
+              <div className="sell-input">
+                <DatePicker
+                  className="datepicker"
+                  dateFormat="yyyy년 MM월 dd일"
+                  locale="ko"
+                  selected={this.state.first_date}
+                  onChange={(first_date) => { this.setState({ first_date }) }}
+                />
+              </div>
+            </div>
+            <div className="sell-list">
+              <label className="sell-label">제조일자</label>
+              <div className="sell-input">
+                <DatePicker
+                  className="datepicker"
+                  dateFormat="yyyy년 MM월 dd일"
+                  locale="ko"
+                  selected={this.state.first_date}
+                  onChange={(first_date) => { this.setState({ first_date }) }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <Button color="primary" style={{width:"100%"}} onClick={() => {this.createStock();}}>등록하기</Button>
+      {/*<link rel="stylesheet" type="text/css" href="css/Table.css"></link>
       <link rel="stylesheet" type="text/css" href="css/Stock.css"></link>
         <Row className="mb-5 justify-content-center">
           <Col md="9" lg="9" xl="8">
@@ -188,8 +267,8 @@ class Create extends Component {
 							</CardFooter>
 						</Card>
 					</Col>
-        </Row>
-      </div>
+        </Row>*/}
+        </div>
     )
   }
 }
