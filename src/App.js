2693e821cbac2e8f65d6e38cdd2c0d00de3d07cb
sowish23 <soso2266@naver.com>
2020-07-24 16:53:05 +0900

    페이지 전체 scrolltotop 적용

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 // import { renderRoutes } from 'react-router-config';
 import './App.scss';
-import './css/Font.css'
+import './css/Font.css';
+import ScrollToTop from './ScrollToTop';
 
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
 
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <HashRouter>
         <React.Suspense fallback={loading()}>
+          <ScrollToTop/>
           <Switch>
             <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
             <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
