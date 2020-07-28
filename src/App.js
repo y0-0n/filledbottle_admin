2bb4f709045210362fbe23b188b2be0da3452ea4
sowish23 <soso2266@naver.com>
2020-07-28 18:00:16 +0900

    랜딩페이지

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const Login = React.lazy(() => import('./views/Pages/Login'));
 const Register = React.lazy(() => import('./views/Pages/Register'));
 const Page404 = React.lazy(() => import('./views/Pages/Page404'));
 const Page500 = React.lazy(() => import('./views/Pages/Page500'));
+const Landing = React.lazy(() => import('./views/LandingPage/LandingPage'));
 
 class App extends Component {
   render() {
@@ -29,7 +30,8 @@ class App extends Component {
 						<Route path="/admin" name="Register Page" render={props => <AdminLayout {...props}/>} />
             <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
             <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-            <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
+            <Route path="/main/home" name="Home" render={props => <DefaultLayout {...props}/>} />
+            <Route path="/" name="LandingPage" render={props => <Landing {...props}/>} />
           </Switch>
         </React.Suspense>
       </HashRouter>
