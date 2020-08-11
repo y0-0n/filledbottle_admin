317d6377fba8cfacfc2dce6fa84a45246e460004
sowish23 <soso2266@naver.com>
2020-08-11 18:13:56 +0900

    add admin router

--- src/routesAdmin.js
+++ src/routesAdmin.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Home = React.lazy(() => import('./views/Admin/Home/Home'));
+const User = React.lazy(() => import('./components/admin/User.jsx'));
+const CreateUser = React.lazy(() => import('./components/admin/CreateUser.jsx'));
+const Farm = React.lazy(() => import('./components/admin/Farm.jsx'));
 const SuggestionList = React.lazy(() => import('./views/Admin/Suggestion/List'));
 const SuggestionDetail = React.lazy(() => import('./views/Admin/Suggestion/Detail'));
 const UserList = React.lazy(() => import('./views/Admin/Users/List'));
@@ -9,7 +12,9 @@ const ProductList = React.lazy(() => import('./views/Admin/Products/List'));
 
 const routes = [
 	{ path: '/admin/home', name: '홈', component: Home},
-	{ path: '/admin/users/list', name: '회원 목록', component: UserList},
+	{ path: '/admin/users/list', name: '회원 목록', component: User},
+	{ path: '/admin/users/create', name: '회원 목록', component: CreateUser},
+	{ path: '/admin/farm/list', name: '농장 목록', component: Farm},
 	{ path: '/admin/users/detail/:id', name: '회원 목록 상세', component: UserListDetail},
 	{ path: '/admin/product/list', name: '품목 목록', component: ProductList},
 	{ path: '/admin/suggestion/list', name: '건의사항', component: SuggestionList},
