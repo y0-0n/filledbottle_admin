0c3bc4cdfec05500563d0072f30c95965efc40d8
sowish23 <soso2266@naver.com>
2020-08-11 18:12:18 +0900

    어드민 회원목록 css 변경

--- src/views/Admin/Users/List.js
+++ src/views/Admin/Users/List.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, CardFooter, Col, Row, Table, Pagination, PaginationItem, PaginationLink} from 'reactstrap';
+import { Card, CardBody, CardHeader, CardFooter, Col, Row, Table, Pagination, PaginationItem, PaginationLink, Button } from 'reactstrap';
 
 const listCount = 15;
 
@@ -7,26 +7,26 @@ class UserList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-			userData : [],
-			page: 1,
+      userData: [],
+      page: 1,
     };
   }
 
   componentWillMount() {
-		this.getList();
-	}
-	
-	getList() {
-		const {page} = this.state;
-		
+    this.getList();
+  }
+
+  getList() {
+    const { page } = this.state;
+
     fetch(process.env.REACT_APP_HOST + "/api/admin/users/list/", {
       method: 'POST',
       headers: {
-				'Accept': 'application/json',
+        'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + localStorage.getItem('token'),
-			},
-			body: JSON.stringify({page})
+      },
+      body: JSON.stringify({ page })
     })
       .then(response => {
         if (response.status === 401) {
@@ -45,8 +45,8 @@ class UserList extends Component {
         }
         this.getTotal();
       })
-	}
-	
+  }
+
   getTotal() {
     fetch(process.env.REACT_APP_HOST + "/api/admin/users/total/", {
       method: 'GET',
@@ -80,71 +80,71 @@ class UserList extends Component {
   }
 
   render() {
-		const arr = [-2, -1, 0, 1, 2];
+    const arr = [-2, -1, 0, 1, 2];
     const arr1 = [];
 
     return (
       <div className="animated fadeIn">
-        <Row className="mb-5">
-          <Col md="12" xs="12" sm="12">
-            <Card>
-              <CardHeader>
-                회원 목록
-              </CardHeader>
-              <CardBody>
-                <Table>
-                  <tbody>
-                    <tr style={{backgroundColor: "#F5F5F5"}}>
-                      <th>#</th>
-                      <th>이름</th>
-                      <th>아이디( 이메일 )</th>
-                      <th>전화번호</th>
-                      <th>주소</th>
-                      <th>사업자 등록번호</th>
+        <link rel="stylesheet" type="text/css" href="css/ListCopy.css"></link>
+        <div className="list-card">
+          <div className="list-title">
+            <span>
+              회원 목록
+            </span>
+            <div style={{ float: "right" }}>
+              <Button style={{ marginBottom: 10 }} onClick={() => { this.props.history.push('/customer/create'); }} color="primary">회원 등록</Button>
+            </div>
+          </div>
+          <div style={{marginTop: 10}} className="list-box">
+            <Table>
+              <tbody>
+                <tr style={{ backgroundColor: "#F5F5F5" }}>
+                  <th>#</th>
+                  <th>이름</th>
+                  <th>아이디( 이메일 )</th>
+                  <th>전화번호</th>
+                  <th>주소</th>
+                  <th>사업자 등록번호</th>
+                </tr>
+                {
+                  this.state.userData.map((e, i) => {
+                    return <tr key={i} onClick={() => { this.props.history.push('/admin/users/detail/' + e.id) }}>
+                      <td>{e.id}</td>
+                      <td>{e.name}</td>
+                      <td>{e.email}</td>
+                      <td>{e.phone}</td>
+                      <td>{e.address}</td>
+                      <td>{e.crNumber}</td>
                     </tr>
-                      {
-                        this.state.userData.map((e, i) => {
-                          return <tr key={i} onClick={() => {this.props.history.push('/admin/users/detail/'+e.id)}}>
-                            <td>{e.id}</td>
-                            <td>{e.name}</td>
-                            <td>{e.email}</td>
-                            <td>{e.phone}</td>
-                            <td>{e.address}</td>
-                            <td>{e.crNumber}</td>
-                          </tr>
-                        })
-                      }
-                  </tbody>
-                </Table>
-              </CardBody>
-							<CardFooter>
-								<Pagination style={{ justifyContent: 'center' }}>
-									{this.state.page === 1 ? '' :
-										<PaginationItem>
-											<PaginationLink previous onClick={() => { this.countPageNumber(this.state.page - 1) }} />
-										</PaginationItem>
-									}
-									{this.state.page === 1 ? arr.forEach(x => arr1.push(x + 2)) : null}
-									{this.state.page === 2 ? arr.forEach(x => arr1.push(x + 1)) : null}
-									{this.state.page !== 1 && this.state.page !== 2 ? arr.forEach(x => arr1.push(x)) : null}
-									{arr1.map((e, i) => {
-										if (this.state.total >= this.state.page + e)
-											return (<PaginationItem key={i} active={this.state.page === this.state.page + e}>
-												<PaginationLink onClick={() => { this.countPageNumber(this.state.page + e) }}>
-													{this.state.page + e}
-												</PaginationLink>
-											</PaginationItem>)
-										return null;
-									})}
-									{this.state.page === this.state.total ? '' :
-										<PaginationItem>
-											<PaginationLink next onClick={() => { this.countPageNumber(this.state.page + 1) }} />
-										</PaginationItem>}
-								</Pagination>
-							</CardFooter>
-            </Card>
-          </Col>
-        </Row>
+                  })
+                }
+              </tbody>
+            </Table>
+            <Pagination style={{ justifyContent: 'center' }}>
+              {this.state.page === 1 ? '' :
+                <PaginationItem>
+                  <PaginationLink previous onClick={() => { this.countPageNumber(this.state.page - 1) }} />
+                </PaginationItem>
+              }
+              {this.state.page === 1 ? arr.forEach(x => arr1.push(x + 2)) : null}
+              {this.state.page === 2 ? arr.forEach(x => arr1.push(x + 1)) : null}
+              {this.state.page !== 1 && this.state.page !== 2 ? arr.forEach(x => arr1.push(x)) : null}
+              {arr1.map((e, i) => {
+                if (this.state.total >= this.state.page + e)
+                  return (<PaginationItem key={i} active={this.state.page === this.state.page + e}>
+                    <PaginationLink onClick={() => { this.countPageNumber(this.state.page + e) }}>
+                      {this.state.page + e}
+                    </PaginationLink>
+                  </PaginationItem>)
+                return null;
+              })}
+              {this.state.page === this.state.total ? '' :
+                <PaginationItem>
+                  <PaginationLink next onClick={() => { this.countPageNumber(this.state.page + 1) }} />
+                </PaginationItem>}
+            </Pagination>
+          </div>
+        </div>
       </div>
     )
 
