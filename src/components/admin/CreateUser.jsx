8e244ca55617c91b2478320d45212bf3fd5d0886
sowish23 <soso2266@naver.com>
2020-08-13 17:58:42 +0900

    회원 등록

--- src/components/admin/CreateUser.jsx
+++ src/components/admin/CreateUser.jsx
@@ -1,33 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { Button, Card, CardBody, CardFooter, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import { useHistory } from 'react-router-dom'
-import { sample6_execDaumPostcode } from './CreateUser'
 
 const CreateUser = () => {
   const history = useHistory()
   const [form, setForm] = useState([])
   const [formName, setFormName] = useState("");
+  const [formFarmName, setFormFarmName] = useState("");
   const [formEmail, setFormEmail] = useState("");
   const [formPassword, setFormPassword] = useState("");
   const [formPasswordC, setFormPasswordC] = useState("");
   const [formPhone, setFormPhone] = useState("");
-  const [formAddress, setFormAddress] = useState("");
-  const [formAddressDetail, setFormAddressDetail] = useState("");
-  const [formPostcode, setFormPostcode] = useState("");
-  const [formCrNumber, setFormCrNumber] = useState("");
-  // const [formAccountName, setFormAccountName] = useState("");
-  // const [formAccountNumber, setFormAccountNumber] = useState("");
 
   function signup() {
     if (formPassword !== formPasswordC) {
       alert('비밀번호와 비밀번호 반복이 일치하는지 확인해주세요');
       return;
     }
-    if (form.address === '' || form.postcode === '') {
-      alert('주소를 확인해주세요');
-      return;
-    }
-
     fetch(process.env.REACT_APP_HOST + "/api/auth/signup", {
       method: 'POST',
       headers: {
@@ -70,22 +59,19 @@ const CreateUser = () => {
     setForm(
       {
         name: formName,
+        farmName: formFarmName,
         email: formEmail,
         password: formPassword,
         passwordC: formPasswordC,
         phone: formPhone,
-        address: document.getElementById("sample6_address").value,
-        addressDetail: formAddressDetail,
-        postcode: document.getElementById("sample6_postcode").value,
-        crNumber: formCrNumber,
       }
     );
-  }, [formName, formEmail, formPassword, formPasswordC, formPhone, formAddress, formAddressDetail, formPostcode, formCrNumber])
+  }, [formName, formEmail, formPassword, formPasswordC, formPhone, formFarmName])
 
   return (
     <div className="animated fadeIn">
       <link rel="stylesheet" type="text/css" href="css/ListCopy.css"></link>
-      <Card style={{ height: '700px' }}>
+      <Card>
         <CardBody className="p-4">
           <h1>회원 등록</h1>
           <p className="text-muted">정보를 입력하세요</p>
@@ -95,13 +81,13 @@ const CreateUser = () => {
                 <i className="icon-user"></i>
               </InputGroupText>
             </InputGroupAddon>
-            <Input type="text" onChange={(e) => {setFormName(e.target.value)}} placeholder="이름" autoComplete="username" />
+            <Input type="text" onChange={(e) => {setFormName(e.target.value)}} placeholder="회원 이름" autoComplete="name" />
           </InputGroup>
           <InputGroup className="mb-3">
             <InputGroupAddon addonType="prepend">
               <InputGroupText>@</InputGroupText>
             </InputGroupAddon>
-            <Input type="text" onChange={(e) => {setFormEmail(e.target.value)}} placeholder="아이디" autoComplete="email" />
+            <Input type="text" onChange={(e) => {setFormEmail(e.target.value)}} placeholder="아이디 (이메일)" autoComplete="email" />
           </InputGroup>
           <InputGroup className="mb-3">
             <InputGroupAddon addonType="prepend">
@@ -121,42 +107,26 @@ const CreateUser = () => {
           </InputGroup>
           <InputGroup className="mb-3">
             <InputGroupAddon addonType="prepend">
-              <InputGroupText>@</InputGroupText>
+              <InputGroupText>
+                <i className="icon-user"></i>
+              </InputGroupText>
             </InputGroupAddon>
-            <Input type="text" onChange={(e) => setFormCrNumber(e.target.value)} placeholder="사업자등록번호" autoComplete="crNumber" />
+            <Input type="text" onChange={(e) => {setFormFarmName(e.target.value)}} placeholder="근무 농장" autoComplete="farmName" />
           </InputGroup>
-          {/* <InputGroup className="mb-3">
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>*</InputGroupText>
-            </InputGroupAddon>
-            <Input type="text" onChange={(e) => setFormAccountName(e.target.value)} placeholder="은행명" autoComplete="AccountNumber" />
-          </InputGroup> */}
-          {/* <InputGroup className="mb-3">
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>*</InputGroupText>
-            </InputGroupAddon>
-            <Input type="text" onChange={(e) => setFormAccountNumber(e.target.value)} placeholder="계좌번호" autoComplete="AccountNumber" />
-          </InputGroup> */}
           <InputGroup className="mb-3">
             <InputGroupAddon addonType="prepend">
               <InputGroupText>#&nbsp;</InputGroupText>
             </InputGroupAddon>
-            <Input type="text" onChange={(e) => setFormPhone(e.target.value)} placeholder="전화번호" autoComplete="phone" />
-          </InputGroup>
-          <InputGroup style={{ width: '60%' }} className="mb-3">
-            <Input type="text" id="sample6_postcode" placeholder="우편번호" onChange={(e) => setFormPostcode(e.target.value)}  readOnly />
-            <InputGroupAddon addonType="append">
-              <Button block color="primary" onClick={sample6_execDaumPostcode}>우편번호찾기</Button>
-            </InputGroupAddon>
+            <Input type="text" onChange={(e) => setFormPhone(e.target.value)} placeholder="연락처" autoComplete="phone" />
           </InputGroup>
           <InputGroup className="mb-3">
-            <Input className="mr-2" type="text" id="sample6_address" placeholder="주소" onChange={(e) => setFormAddress(e.target.value)} readOnly />
-            <Input type="text" id="sample6_detailAddress" placeholder="상세주소" onChange={(e) => setFormAddressDetail(e.target.value)} />
+            <InputGroupAddon addonType="prepend">
+              <InputGroupText>#&nbsp;</InputGroupText>
+            </InputGroupAddon>
+            <Input type="text" onChange={(e) => setFormPhone(e.target.value)} placeholder="대표 / 직원" autoComplete="phone" />
           </InputGroup>
           <Button color="success" id="button_joinus" disabled=""block onClick={signup}>등록하기</Button>
         </CardBody>
-        <CardFooter className="p-4">
-        </CardFooter>
       </Card>
     </div>
   );
--- src/components/admin/User.jsx
+++ src/components/admin/User.jsx
@@ -50,9 +50,7 @@ const User = () => {
       <link rel="stylesheet" type="text/css" href="css/ListCopy.css"></link>
       <div className="list-card">
         <div className="list-title">
-          <span>
-            회원 목록
-            </span>
+          <span>회원 목록</span>
           <div style={{ float: "right" }}>
             <Button style={{ marginBottom: 10 }} onClick={() => { history.push('/admin/users/create'); }} color="primary">회원 등록</Button>
           </div>
